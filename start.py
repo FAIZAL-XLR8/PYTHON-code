c912d9912e0aa52136c2719f2e11f9d2ebc3dcd4
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-02 21:28:57 +0530

    if and else

--- start.py
+++ start.py
@@ -3,3 +3,18 @@ print("let the show begin babes!")
 this is a comment and is not interpreted by the interpreter
 """
 #for single line we opt for hash
+hello = "alpha"
+print(hello)
+print(type(hello))
+hello = 12
+print(hello)
+print(type(hello))
+if (5 > 4):
+    print(hello)
+else:
+    print("Barclays")
+print("The show is over")
+    
+saar = 10
+hello_bro = "I know everything"
+print(hello_bro)
