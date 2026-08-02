42024e9b121a0d6a45eff789f856ec044567e3f6
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-02 22:24:18 +0530

    added string usages

--- second.py
+++ second.py
@@ -2,3 +2,11 @@ str = "a"
 print(str)
 print(ord(str))
 
+"""
+here we need to note that 
+ord is for the ascii values but the 
+chr is for the character changes
+like chr(65) will result in A
+"""
+print("here is the use of chr", "hello ")
+print(chr(65))
