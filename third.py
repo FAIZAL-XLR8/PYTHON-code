85fd338b6ec8dea766ab4a0abe15e994af34bf3e
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-03 00:45:04 +0530

    added third

--- third.py
+++ third.py
@@ -1,6 +1,10 @@
+#this is type conversion
 variable_1 = "100"
 var2 = 10.220
 var3 = 97
-print(float(variable_1))
-print(str(var2))
-print(chr(var3))
+print(float(variable_1))  #this prints 100.0
+print(str(var2))  #this prints "10.220"
+print(chr(var3))  #this prints "a"
+var4 = float(variable_1)  #this prints 100.0
+print(type(var4)) #this prints <class 'float'>
+print(type(variable_1)) #this prints <class 'str'>
