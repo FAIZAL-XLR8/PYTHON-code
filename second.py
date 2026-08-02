80f3e516318fdc41d72510d0806ece0bae646eeb
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-02 22:37:21 +0530

    Added string slicing

--- second.py
+++ second.py
@@ -10,3 +10,20 @@ like chr(65) will result in A
 """
 print("here is the use of chr", "hello ")
 print(chr(65))
+aizen = "aizen"
+print(aizen[0], aizen[2], aizen[4])
+
+"""
+slicing of the string
+
+variable_name[start_idx : end_idx : step] (end_idx not included)
+step is completely optional and by default taken as 1
+
+start_idx starts from 0 and
+end idx can also be represnted as -1 to -n from right to left
+-4 -3 -2 -1 
+
+ """
+aizen = "aizen"
+
+print(aizen[-4 : -1])
