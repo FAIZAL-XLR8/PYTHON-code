532087e4d249d694048739b0482ac9eb04876495
FAIZAL-XLR8 <slif858@gmail.com>
2026-08-03 12:12:48 +0530

    added input format

---
+++ sixth.py
@@ -0,0 +1,5 @@
+#taking input from a user
+message = input("what brings uh here ? ")
+#input by default takes string from the user for type conversion
+#int(input("message"))
+print("the message by the user is", message)
